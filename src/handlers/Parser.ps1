3323594a70bf5216489ac858ff521c6b84f7801d
Lyon Till <lyon.till@microsoft.com>
2023-10-24 02:02:39 +0100

    feat: update global parameters

---
+++ src/handlers/Parser.ps1
@@ -0,0 +1,15 @@
+function global:Parser {
+    param(
+        [Parameter(Mandatory)]
+        $cmdResults
+    )
+
+    $data = ($cmdResults | Out-String)
+
+    if (Test-Json -Json $data -ErrorAction SilentlyContinue) {
+        $data | ConvertFrom-Json
+    }
+    else {
+        Write-Output $data
+    }
+}
